3ebdd5a59557dec01b272f5a3a25c76afdcc9e43
sebprt <sebastien.parrat@orange.fr>
2021-09-21 17:32:20 +0200

    Updated workflow to use jobs

--- src/Workflow.php
+++ src/Workflow.php
@@ -3,21 +3,26 @@
 namespace Kiboko\Component\Workflow;
 
 use Kiboko\Contract\Pipeline\PipelineInterface;
+use Kiboko\Contract\Pipeline\RunnableInterface;
 
 class Workflow implements RunnableInterface
 {
-    private iterable $pipelines;
+    /** @var list<PipelineInterface> */
+    private array $jobs = [];
 
-    public function __construct(
-        PipelineInterface ...$pipelines
-    ) {
-        $this->pipelines = $pipelines;
+    public function job(RunnableInterface $job): self
+    {
+        $this->jobs[] = $job;
+
+        return $this;
     }
 
-    public function run(): void
+    public function run(): int
     {
-        foreach ($this->pipelines as $pipeline) {
-            $pipeline->run();
+        foreach ($this->jobs as $job) {
+            $job->run();
         }
+
+        return 0;
     }
 }
